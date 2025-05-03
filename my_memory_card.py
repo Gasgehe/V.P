c407251ae95392bb4a1b09e006cb5631df9cde05
Gasgehe <abonka76@gmail.com>
2025-05-03 14:14:20 +0300

    Add files via upload

---
+++ my_memory_card.py
@@ -0,0 +1,45 @@
+#создай приложение для запоминания информации
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+QApplication, QWidget, QRadioButton,
+QLabel, QGroupBox, QPushButton,
+QVBoxLayout, QHBoxLayout, 
+QButtonGroup
+)
+from random import shuffle, randint
+
+
+# class Victorin():
+#     def __init__(self, , , , , ):
+#         self. = 
+#         self. = 
+#         self. =
+#         self. = 
+#         self. = 
+
+
+# создание окна
+app = QApplication([])
+main_win = QWidget()
+
+main_win.resize(500, 500)
+main_win.setWindowTitle('')
+
+#интерфейс
+Victorin = QLabel('Викторины')
+r_group_box = QGroupBox('Выберите викторину')
+btn_Victorin = QPushButton('Викторина по играм')
+btn_Victorin2 = QPushButton('Викторина по фильмам')
+btn_Victorin3 = QPushButton('Викторина по.....')
+
+#Виджеты
+# layout1 = QHBoxLayout()
+# layout2 = QVBoxLayout()
+# layout3 = QVBoxLayout()
+
+# layout2.addWidget()
+# layout2.addWidget()
+# layout3.addWidget()
+
+main_win.show()
+app.exec_()
